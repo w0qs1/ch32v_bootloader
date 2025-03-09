94a3bbb39b43db0c30f65f4a479083bcd0246ecf
w0qs1 <mnsandeepkumar@gmail.com>
2025-03-09 10:54:27 +0530

    fix

--- User/test.c
+++ User/test.c
@@ -1,9 +1,9 @@
 #include <stdint.h>
 
-void test(void) {
+uint32_t test(void) {
     volatile uint32_t *a;
     volatile uint32_t c;
     a = (uint32_t *) 0x00000000;
     c = *(a) + 1;
-    return;
+    return c;
 }
--- obj/Startup/startup_ch32v00x.d
+++ obj/Startup/startup_ch32v00x.d
@@ -1,4 +1,1 @@
-Startup/startup_ch32v00x.o: ../Startup/startup_ch32v00x.S \
- ../Startup/boot_config.S
-
-../Startup/boot_config.S:
+Startup/startup_ch32v00x.o: ../Startup/startup_ch32v00x.S
