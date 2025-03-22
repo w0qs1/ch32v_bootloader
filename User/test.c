42a6cc3941a3db4aff7523a64c691733b073e235
w0qs1 <mnsandeepkumar@gmail.com>
2025-03-22 17:00:33 +0530

    Separated Bootloader and application section for fw upgrade

--- User/test.c
+++
@@ -1,9 +0,0 @@
-#include <stdint.h>
-
-uint32_t test(void) {
-    volatile uint32_t *a;
-    volatile uint32_t c;
-    a = (uint32_t *) 0x00000000;
-    c = *(a) + 1;
-    return c;
-}
--- obj/User/subdir.mk
+++ obj/User/subdir.mk
@@ -7,20 +7,17 @@
 C_SRCS += \
 ../User/ch32v00x_it.c \
 ../User/main.c \
-../User/system_ch32v00x.c \
-../User/test.c 
+../User/system_ch32v00x.c 
 
 C_DEPS += \
 ./User/ch32v00x_it.d \
 ./User/main.d \
-./User/system_ch32v00x.d \
-./User/test.d 
+./User/system_ch32v00x.d 
 
 OBJS += \
 ./User/ch32v00x_it.o \
 ./User/main.o \
-./User/system_ch32v00x.o \
-./User/test.o 
+./User/system_ch32v00x.o 
 
 
 
